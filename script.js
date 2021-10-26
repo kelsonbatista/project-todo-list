3136649b556d095733af90935cf858e3716b2de4
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-10-26 18:50:09 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -2,6 +2,16 @@ const inputTask = document.querySelector('#texto-tarefa');
 const btnAddTask = document.querySelector('#criar-tarefa');
 const todoList = document.getElementById('lista-tarefas');
 
+function selectItem(event) {
+  // const itemId = event.target.id;
+  const itemList = document.querySelectorAll('.todolist__item');
+  for (let i = 0; i < itemList.length; i += 1) {
+    itemList[i].style.backgroundColor = '';
+  }
+  const evt = event.target;
+  evt.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
 function addTask() {
   const lastItem = todoList.lastChild;
   const todoItem = document.createElement('li');
@@ -16,9 +26,7 @@ function addTask() {
   inputTask.value = '';
   const todoItemSelect = document.querySelectorAll('.todolist__item');
   for (let i = 0; i < todoItemSelect.length; i += 1) {
-    todoItemSelect[i].addEventListener('click', () => {
-      todoItemSelect[i].style.backgroundColor = 'rgb(128, 128, 128)';
-    });
+    todoItemSelect[i].addEventListener('click', selectItem);
   }
 }
 btnAddTask.addEventListener('click', addTask);
