4e16a6b08ad2f4d59bcd8085d4b7e702bed37305
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-10-26 17:32:35 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
+const inputTask = document.querySelector('#texto-tarefa');
+const btnAddTask = document.querySelector('#criar-tarefa');
+const todoList = document.getElementById('lista-tarefas');
+
 function addTask() {
-  const inputTask = document.querySelector('#texto-tarefa');
+  const lastItem = todoList.lastChild;
   const todoItem = document.createElement('li');
   todoItem.className = 'todolist__item';
+  if (Number.isNaN(lastItem.id)) {
+    todoItem.id = 1;
+  } else {
+    todoItem.id = Number(lastItem.id) + 1;
+  }
   todoItem.innerText = inputTask.value;
-  const todoList = document.querySelector('#lista-tarefas');
   todoList.appendChild(todoItem);
   inputTask.value = '';
+  const todoItemSelect = document.querySelectorAll('.todolist__item');
+  for (let i = 0; i < todoItemSelect.length; i += 1) {
+    todoItemSelect[i].addEventListener('click', () => {
+      todoItemSelect[i].style.backgroundColor = 'rgb(128, 128, 128)';
+    });
+  }
 }
-
-function init() {
-  const btnAddTask = document.querySelector('#criar-tarefa');
-  btnAddTask.addEventListener('click', addTask);
-}
-
-init();
+btnAddTask.addEventListener('click', addTask);
