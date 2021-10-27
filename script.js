a2d05499a375e9fbb874d552765c90aa2a4e2b62
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-10-27 00:11:54 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -1,32 +1,37 @@
 const inputTask = document.querySelector('#texto-tarefa');
 const btnAddTask = document.querySelector('#criar-tarefa');
-const todoList = document.getElementById('lista-tarefas');
 
 function selectItem(event) {
-  // const itemId = event.target.id;
-  const itemList = document.querySelectorAll('.todolist__item');
-  for (let i = 0; i < itemList.length; i += 1) {
-    itemList[i].style.backgroundColor = '';
-  }
   const evt = event.target;
-  evt.style.backgroundColor = 'rgb(128, 128, 128)';
+  if (evt.className !== 'todolist__item completed') {
+    const itemList = document.getElementsByClassName('todolist__item');
+    for (let i = 0; i < itemList.length; i += 1) {
+      itemList[i].style.cssText = '';
+    }
+    evt.style.cssText += 'background-color: rgb(128, 128, 128);';
+  }
+}
+
+function dblClickItem(event) {
+  const evt = event.target;
+  if (evt.classList.contains('completed')) {
+    evt.classList.remove('completed');
+  } else {
+    evt.classList.add('completed');
+  }
 }
 
 function addTask() {
-  const lastItem = todoList.lastChild;
+  const todoList = document.getElementById('lista-tarefas');
   const todoItem = document.createElement('li');
   todoItem.className = 'todolist__item';
-  if (Number.isNaN(lastItem.id)) {
-    todoItem.id = 1;
-  } else {
-    todoItem.id = Number(lastItem.id) + 1;
-  }
   todoItem.innerText = inputTask.value;
   todoList.appendChild(todoItem);
   inputTask.value = '';
-  const todoItemSelect = document.querySelectorAll('.todolist__item');
+  const todoItemSelect = document.getElementsByClassName('todolist__item');
   for (let i = 0; i < todoItemSelect.length; i += 1) {
     todoItemSelect[i].addEventListener('click', selectItem);
+    todoItemSelect[i].addEventListener('dblclick', dblClickItem);
   }
 }
 btnAddTask.addEventListener('click', addTask);
