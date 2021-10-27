f9d4441239e174dc0f28b81c42dc0a9ae57f0bf3
kelsonbatista <kelsonbatista@yahoo.co.uk>
2021-10-27 12:52:50 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -41,6 +41,10 @@ function clearAll() {
   while (todoItemSelect.lastElementChild) {
     todoItemSelect.removeChild(todoItemSelect.lastElementChild);
   }
+  const theList = JSON.parse(localStorage.getItem('list'));
+  if ((theList !== null && theList.length > 0)) {
+    localStorage.removeItem('list');
+  }
 }
 const btnClearAll = document.querySelector('#apaga-tudo');
 btnClearAll.addEventListener('click', clearAll);
@@ -55,3 +59,35 @@ function clearDone() {
 }
 const btnClearDone = document.querySelector('#remover-finalizados');
 btnClearDone.addEventListener('click', clearDone);
+
+function saveList() {
+  const list = [];
+  const todoItemSelect = document.querySelectorAll('.todolist__item');
+  for (let i = 0; i < todoItemSelect.length; i += 1) {
+    list.push({
+      text: todoItemSelect[i].innerText,
+      class: todoItemSelect[i].className,
+    });
+  }
+  localStorage.setItem('list', JSON.stringify(list));
+  alert('Lista salva!');
+}
+const btnSaveList = document.querySelector('#salvar-tarefas');
+btnSaveList.addEventListener('click', saveList);
+
+function showList() {
+  const theList = JSON.parse(localStorage.getItem('list'));
+  console.log(theList);
+  if (theList !== null && theList.length > 0) {
+    const listLength = theList.length;
+    const todoList = document.getElementById('lista-tarefas');
+    for (let i = 0; i < listLength; i += 1) {
+      const todoItem = document.createElement('li');
+      todoItem.className = theList[i].class;
+      todoItem.innerText = theList[i].text;
+      todoList.appendChild(todoItem);
+    }
+  }
+}
+
+showList();
